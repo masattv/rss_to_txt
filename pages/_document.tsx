9ef76eb91d423b62c7614d348e8399c0c98fe0df
Your Name <your-email@example.com>
2025-06-11 04:21:15 +0900

    fix: Google AdSenseスクリプトを追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7578897320709624"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
